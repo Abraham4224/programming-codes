89a51f7dd4b9aeaf0789714fd1274d8bc3eff26a
Abraham4224 <130403977+Abraham4224@users.noreply.github.com>
2024-08-02 13:04:17 -0600

    Suma de dos numeros pedidos al usuario

---
+++ suma.js
@@ -0,0 +1,9 @@
+// Pide al usuario que ingrese dos números
+const numero1 = parseFloat(prompt("Ingresa el primer número:"));
+const numero2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+// Calcula la suma
+const suma = numero1 + numero2;
+
+// Muestra el resultado
+console.log(`La suma de ${numero1} y ${numero2} es ${suma}.`);
