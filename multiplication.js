93d7ef68f33d7dd2b88a10466016f2094bbd3782
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-30 10:02:45 -0600

    Create multiplication.js

---
+++ multiplication.js
@@ -0,0 +1,9 @@
+
+let num1 = parseFloat(prompt("Introduce el primer número:"));
+let num2 = parseFloat(prompt("Introduce el segundo número:"));
+
+
+let mul = num1 * num2;
+
+
+alert("La multiplicación de " + num1 + " y " + num2 + " es: " + mul);
