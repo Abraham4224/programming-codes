42d2ad7b96574d1dcdc100d568c67ca5e81527e3
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-30 10:06:50 -0600

    Create hello-world.cs

---
+++ hello-world.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
