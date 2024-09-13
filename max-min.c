fcf1b442263fbe87f9c119deab20adb1874332c8
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-09-13 10:03:58 -0600

    Create max-min.c

---
+++ max-min.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int first = 1;
+    int val, maxval, minval;
+
+    while (scanf("%d",&val) != EOF)
+    {
+        /* code */
+        if ( first || val > maxval)  maxval = val;
+        if ( first || val < minval)  minval = val;
+        first = 0;
+    } 
+    printf("Maximum %d\n", maxval);
+    printf("Minimum %d\n", minval);
+    return 0;
+}
