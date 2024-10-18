a0927c271c7c59e255861e46b41b2c0cf5e19f71
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-10-18 10:41:01 -0600

    Create Scan.java

---
+++ Scan.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Scan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char caracter;
+		String Julius;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce un numero: ");
+		int numero = entrada.nextInt();
+		System.out.println("Tu numero es: "+numero);
+		entrada.nextLine();
+		System.out.println("Introduce un chr: ");
+		caracter = entrada.next().charAt(0);
+		System.out.println("El caracter es "+caracter);
+		entrada.nextLine();
+        
+		System.out.println("Introduce una frase de Julio Cesar: ");
+		Julius = entrada.nextLine();
+		System.out.println("La frase es: "+Julius);
+		
+		
+		entrada.close();
+	}
+
+}
