b2d76d76166a600c7bc47f6583c39e1616e9d2d8
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-10-23 11:01:54 -0600

    Create array.java

---
+++ array.java
@@ -0,0 +1,17 @@
+public class Dodo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boolean deedee = false;
+		do {
+			System.out.println("Se imprimio, esto no pasaria en un bucle while");
+		} while(deedee);
+		
+		int[] arrayNumeros = new int[10];
+		for(int i = 0; i < 10; i++) {
+			arrayNumeros[i] = i;
+		}
+		char[] eo = {'J','G','C'};
+	}
+
+}
