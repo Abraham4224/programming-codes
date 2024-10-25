b218bc5d25323fceca3fa353b4974725a09da7fe
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-10-25 14:11:47 -0600

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Imprimo por consola: ");
+		Edificio titan = new Edificio();
+		
+	}
+
+}
