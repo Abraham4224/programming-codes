623ee78c19e6ac8544a658ab96c56a1f1bd05283
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-10-28 17:02:30 -0600

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,8 +4,13 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Imprimo por consola: ");
-		Edificio titan = new Edificio();
-		
+		Edificio AG = new Edificio();
+		Edificio titan = new Edificio(44.4,4,'A',1999);
+		titan.construyendo();
+		AG.destruir();
+		System.out.println(titan.getAltura());
+		titan.setAltura(44.42);
+		System.out.println(titan.getAltura());
 	}
 
 }
