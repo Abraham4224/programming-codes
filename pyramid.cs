d0d1adde6169710118d32daea9da691704fe89bc
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-11-20 13:52:45 -0600

    Create pyramid.cs

---
+++ pyramid.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        // Número de escalones
+        int escalones = 3;
+
+        // Dibuja cada escalón
+        for (int i = 1; i <= escalones; i++)
+        {
+            // Espacios antes de cada estrella
+            for (int j = 1; j <= escalones - i; j++)
+            {
+                Console.Write(" ");
+            }
+            
+            // Dibuja las estrellas en cada escalón
+            for (int j = 1; j <= (2 * i - 1); j++)
+            {
+                Console.Write("*");
+            }
+            
+            // Salto de línea después de cada escalón
+            Console.WriteLine();
+        }
+    }
+}
