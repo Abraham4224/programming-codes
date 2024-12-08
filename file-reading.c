d83235f31f40add3b498e40b6a387f4303df53e8
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-12-08 13:26:59 -0600

    Update file-reading.c

--- file-reading.c
+++ file-reading.c
@@ -3,7 +3,7 @@
 int main()
 {
     char line[1000];
-    FILE *hand;
+    FILE *hand; // Puntero para manejar el archivo
     hand = fopen("romeo.txt","r");
     while( fgets(line,1000,hand) != NULL ) {
         printf("%s",line);
