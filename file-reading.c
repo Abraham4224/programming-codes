fc0bc21f534a56ae94d2af00c2dd8d1382cc33b4
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-12-13 11:31:10 -0600

    Update file-reading.c

--- file-reading.c
+++ file-reading.c
@@ -3,7 +3,7 @@
 int main()
 {
     char line[1000]; // Array to store a line from the file
-    FILE *hand; // Puntero para manejar el archivo
+    FILE *hand; // Pointer to manage the file
     hand = fopen("romeo.txt","r");
     while( fgets(line,1000,hand) != NULL ) {
         printf("%s",line);
